55edb7e001745753589f95c5ab50c97905e82d11
Felipe <felipe@talavera.io>
2020-12-06 21:50:02 -0300

    adding spring security

--- src/main/java/cl/talavera/challenge/persistence/JWT.java
+++ src/main/java/cl/talavera/challenge/persistence/JWT.java
@@ -1,10 +1,19 @@
 package cl.talavera.challenge.persistence;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
 
 @Service
 public class JWT {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public String generate(String password) {
-        return password;
+        return bCryptPasswordEncoder.encode(password);
     }
 }
