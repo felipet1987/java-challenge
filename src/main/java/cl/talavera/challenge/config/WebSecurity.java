0fb2e427619b652ae83bd898647c23b2e4088d54
Felipe <felipe@talavera.io>
2020-12-06 21:52:08 -0300

    cleaning

--- src/main/java/cl/talavera/challenge/config/WebSecurity.java
+++ src/main/java/cl/talavera/challenge/config/WebSecurity.java
@@ -22,13 +22,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        /*
-         * 1. Se desactiva el uso de cookies
-         * 2. Se activa la configuración CORS con los valores por defecto
-         * 3. Se desactiva el filtro CSRF
-         * 4. Se indica que el login no requiere autenticación
-         * 5. Se indica que el resto de URLs esten securizadas
-         */
         httpSecurity
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .cors().and()
